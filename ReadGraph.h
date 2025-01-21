2dc5979c454243079f16d90988824ef91c8946ba
Jan Larmann <github5403asiunbb@jlar.simplelogin.com>
2025-01-21 12:31:16 -0500

    remove unused types

--- ReadGraph.h
+++ ReadGraph.h
@@ -4,20 +4,6 @@
 #include <string>
 #include <unordered_map>
 
-struct Edge {
-    std::string destinationNodeIdentifier{};
-    int edgeWeight{};
-
-    bool operator<(const Edge &other) const {
-        return destinationNodeIdentifier < other.destinationNodeIdentifier;
-    }
-};
-
-enum class GraphEdgeType {
-    undirected,
-    directed
-};
-
 /**
  * Reads an adjacency list from a file.
  * @param path The path to the adjacency list
